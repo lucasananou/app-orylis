d97070d80c9a7cb34cce762fd81ea920488eec4c
lucasananou <lucasananou@gmail.com>
2025-12-01 17:36:37 +0400

    Add Meta Pixel ViewContent event to start page

--- app/start/page.tsx
+++ app/start/page.tsx
@@ -1,9 +1,14 @@
+import Script from "next/script";
 import { PublicOnboardingWizard } from "@/components/onboarding/public-onboarding-wizard";
 
 export default function StartPage() {
     return (
         <div className="min-h-screen bg-white">
             <PublicOnboardingWizard />
+            {/* Facebook Pixel - ViewContent */}
+            <Script id="fb-viewcontent" strategy="afterInteractive">
+                {`if (typeof fbq === 'function') { try { fbq('track', 'ViewContent'); } catch(e) {} }`}
+            </Script>
         </div>
     );
 }
